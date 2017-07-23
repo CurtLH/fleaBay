99bd2d82729a9f518b92f3460f66c52bb711e988
Curtis H <CurtLHampton@gmail.com>
2017-07-23 12:46:38 -0500

    Remove unecessary code from notebook format

--- merge_data.py
+++ merge_data.py
@@ -1,51 +1,87 @@
 #!/usr/bin/env python
+
+import logging
 import psycopg2
 from sqlalchemy import create_engine
 import pandas as pd
 import numpy as np
+import click
 
-# create a connection to get data from database
-engine = create_engine('postgresql://postgres:apassword@localhost:5432/postgres')
+# enable logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s',
+                    datefmt="%Y-%m-%d %H:%M:%S")
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-# get API data from the database
-api_df = pd.read_sql('ebay_api', con=engine)
 
-# zap column names into lowercase
-api_df.columns = [col.lower() for col in api_df.columns]
+@click.command()
+def cli():
 
-# reduce df to variables of interest
-api_df = api_df[['itemid', 'condition_conditiondisplayname', 'listinginfo_endtime', 'sellingstatus_currentprice_value', 'sellingstatus_sellingstate']]
+    """
+    Load API data and web data and merge into one dataset
+    """
 
-# rename columns
-api_df = api_df.rename(columns={'condition_conditiondisplayname' : 'condition',
-                                'listinginfo_endtime' : 'endtime',
-                                'sellingstatus_currentprice_value' : 'price',
-                                'sellingstatus_sellingstate' : 'status'})
+    # connect to the databse
+    try:
+        engine = create_engine('postgresql://postgres:apassword@localhost:5432/postgres')
+        logger.info("Successfully connected to the database")
 
-# get item attribute info from the database
-item_df = pd.read_sql('ebay_item_attr', con=engine)
+    except:
+        logger.info("Unable to connect to the database")
 
-# reduce df to variables of interest
-item_df = item_df[['itemId', 'Mileage', 'Year', 'CYL', 'TITLE', 'TRANS', 'TRIM', 'EXT_COLOR']]
+    # get API data from the database
+    api_df = pd.read_sql('ebay_api', con=engine)
 
-# reduce df to the years of interests
-item_df = item_df[item_df['Year'] >= 2010]
-print("Number of Camaros between 2010 and 2017: {}".format(len(item_df)))
+    # zap column names into lowercase
+    api_df.columns = [col.lower() for col in api_df.columns]
 
-# convert column names to lowercase
-item_df.columns = [x.lower() for x in item_df.columns]
+    # reduce df to variables of interest
+    api_df = api_df[['itemid', 
+                     'condition_conditiondisplayname', 
+                     'listinginfo_endtime', 
+                     'sellingstatus_currentprice_value', 
+                     'sellingstatus_sellingstate']]
 
-# for some reason, ads before Jan 18 were all sold ads
-df.drop(df[df['endtime'] < '2017-01-18'].index, inplace=True)
-df.drop(df[df['itemid'] == 122319430122].index, inplace=True)
-df.drop(df[df['itemid'] == 322439374219].index, inplace=True)
-df.drop(df[df['itemid'] == 192121219541].index, inplace=True)
-df.drop(df[df['itemid'] == 222418277892].index, inplace=True)
-df.drop(df[df.endtime.dt.year <= 2016].index, inplace=True)
+    # rename columns
+    api_df = api_df.rename(columns={'condition_conditiondisplayname' : 'condition',
+                                    'listinginfo_endtime' : 'endtime',
+                                    'sellingstatus_currentprice_value' : 'price',
+                                    'sellingstatus_sellingstate' : 'status'})
 
-# create a connection to write df to database
-engine = create_engine('postgresql://postgres:apassword@localhost:5432/postgres')
+    # get item attribute info from the database
+    item_df = pd.read_sql('ebay_item_attr', con=engine)
 
-# load the dataframe into the database
-df.to_sql(name='ebay_merged', con=engine, if_exists = 'replace', chunksize=2500, index=False)
+    # reduce df to variables of interest
+    item_df = item_df[['itemId', 
+                       'Mileage', 
+                       'Year', 
+                       'CYL', 
+                       'TITLE', 
+                       'TRANS', 
+                       'TRIM', 
+                       'EXT_COLOR']]
 
+    # reduce df to the years of interests
+    item_df = item_df[item_df['Year'] >= 2010]
+    logger.info("Number of Camaros between 2010 and 2017: {}".format(len(item_df)))
+
+    # convert column names to lowercase
+    item_df.columns = [x.lower() for x in item_df.columns]
+
+    # for some reason, ads before Jan 18 were all sold ads
+    df.drop(df[df['endtime'] < '2017-01-18'].index, inplace=True)
+    df.drop(df[df['itemid'] == 122319430122].index, inplace=True)
+    df.drop(df[df['itemid'] == 322439374219].index, inplace=True)
+    df.drop(df[df['itemid'] == 192121219541].index, inplace=True)
+    df.drop(df[df['itemid'] == 222418277892].index, inplace=True)
+    df.drop(df[df.endtime.dt.year <= 2016].index, inplace=True)
+
+    # create a connection to write df to database
+    engine = create_engine('postgresql://postgres:apassword@localhost:5432/postgres')
+
+    # load the dataframe into the database
+    df.to_sql(name='ebay_merged', con=engine, if_exists = 'replace', chunksize=2500, index=False)
+
+if __name__ == "__main__":
+    cli()
