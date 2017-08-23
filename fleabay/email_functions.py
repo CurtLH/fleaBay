efd748a7bc3940b13972ca4f2d6704a833486511
Curtis H <CurtLHampton@gmail.com>
2017-08-22 22:28:48 -0500

    Add email status updates (#17)

---
+++ fleabay/email_functions.py
@@ -0,0 +1,37 @@
+import smtplib
+import logging
+
+# enable logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s',
+                    datefmt="%Y-%m-%d %H:%M:%S")
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def send_email(user, pwd, recipient, subject, body):
+
+    """
+    Thanks to #10147455
+    """
+
+    gmail_user = user
+    gmail_pwd = pwd
+    FROM = user
+    TO = recipient if type(recipient) is list else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(gmail_user, gmail_pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        logger.info('successfully sent the mail')
+    except:
+        logger.info('failed to send mail')
